b0205c332368ff30bc6600c6e9e2d4523816988c
Samuel Foltran <contato@samuelfoltran.com>
2022-09-22 20:52:20 -0300

    Created users.js, which is a users database, with post and comments.

---
+++ src/users.js
@@ -0,0 +1,26 @@
+'use strict';
+
+users = [
+    {
+        name: 'Samuel',
+        posts: [
+            {
+                title: 'Hello! This is my first post!',
+                text: "I'm really happy i can talk to you!",
+                likes: 0,
+                comments: [
+                    {text: "Welcome to the world!"}
+                ]
+            },
+            {
+                title: 'Broh, I love pizza!',
+                text: "I Really want to share to you that pizza is the best food",
+                likes: 0,
+                comments: [
+                    {text: "Really man, it is!"},
+                    {text: "I'm not sure..."}
+                ]
+            }
+        ]
+    }
+];
